77f78451538262a439109564f435d40839853c77
prathaban-s <prathaban@codepalette.ai>
2024-01-04 09:51:07 +0530

    Add tiny editor

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { Editor } from "@tinymce/tinymce-react";
 
 import * as z from "zod";
 
@@ -20,6 +21,8 @@ import { Input } from "@/components/ui/input";
 import { QuestionSchema } from "@/lib/validation";
 
 const Question = () => {
+  const editorRef = useRef(null);
+
   const form = useForm<z.infer<typeof QuestionSchema>>({
     resolver: zodResolver(QuestionSchema),
     defaultValues: {
@@ -63,16 +66,49 @@ const Question = () => {
           control={form.control}
           name="explanation"
           render={({ field }) => (
-            <FormItem className="flex w-full flex-col gap-32">
+            <FormItem className="flex w-full flex-col gap-3">
               <FormLabel className="paragraph-semibold text-dark400_light800">
                 Detailed Explanation <span className="text-primary-500">*</span>
               </FormLabel>
-              {/* <FormControl className="mt-3.5">
-                <Input
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                  {...field}
-                />
-              </FormControl> */}
+              <Editor
+                apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                onInit={(evt, editor) =>
+                  // @ts-ignore
+                  (editorRef.current = editor)
+                }
+                initialValue=""
+                init={{
+                  height: 500,
+                  menubar: false,
+                  plugins: [
+                    "advlist",
+                    "autolink",
+                    "lists",
+                    "link",
+                    "image",
+                    "charmap",
+                    "preview",
+                    "anchor",
+                    "searchreplace",
+                    "visualblocks",
+                    "code",
+                    "fullscreen",
+                    "insertdatetime",
+                    "media",
+                    "table",
+                    "code",
+                    "help",
+                    "wordcount",
+                  ],
+                  toolbar:
+                    "undo redo | blocks | " +
+                    "codesample |bold italic forecolor | alignleft aligncenter " +
+                    "alignright alignjustify | bullist numlist | " +
+                    "removeformat | help",
+                  content_style:
+                    "body { font-family:Inetr,Arial,sans-serif; font-size:16px }",
+                }}
+              />
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Itroduce the problem and expand on what you put in the title.
                 Minimum 20 characters.
