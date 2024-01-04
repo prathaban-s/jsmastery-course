9be84b869a1551a56a55706ebeaffa0973ff7163
prathaban-s <prathaban@codepalette.ai>
2024-01-04 22:10:06 +0530

    Deploy clerk webhook

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,13 @@
 
 import User from "@/database/user.model";
 import { connnectToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
 export async function getUserById(params: any) {
   try {
@@ -14,3 +21,53 @@ export async function getUserById(params: any) {
     console.log(err);
   }
 }
+
+export const createUser = async (userData: CreateUserParams) => {
+  try {
+    connnectToDatabase();
+    const newUser = User.create(userData);
+    console.log(newUser);
+    return newUser;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const updateUser = async (userData: UpdateUserParams) => {
+  try {
+    connnectToDatabase();
+
+    const { clerkId, updateData, path } = userData;
+    const existingUser = await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+    revalidatePath(path);
+    return existingUser;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const deleteUser = async (userData: DeleteUserParams) => {
+  try {
+    connnectToDatabase();
+
+    const { clerkId } = userData;
+
+    const user = await User.findOneAndDelete({ clerkId });
+
+    if (!user) {
+      throw new Error("No User Exist");
+    }
+
+    // const userQuestionIds = await Question.find({
+    //   author: user._id,
+    // }).distinct("_id");
+
+    await Question.deleteMany({ author: user._id });
+
+    return user;
+  } catch (err) {
+    console.log(err);
+  }
+};
